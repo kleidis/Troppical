f5a400a13b0d3b81d1992e83a0ae2e637d092ff7
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-05-31 18:58:14 +0530

    Fix errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 android {

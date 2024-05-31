01fa2269bc9e5e909695dd9bfc3017be93ad736b
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-05-31 22:06:30 +0530

    bonk

--- android/app/src/main/java/io/github/troppical/network/APKDownloader.kt
+++ android/app/src/main/java/io/github/troppical/network/APKDownloader.kt
@@ -9,7 +9,7 @@ import java.io.IOException
 
 class APKDownloader(private val url: String, private val outputFile: File) {
 
-    fun download(onComplete: (Boolean) -> Unit) {
+    fun download(onProgress: (Int) -> Unit, onComplete: (Boolean) -> Unit) {
         val client = OkHttpClient()
         val request = Request.Builder().url(url).build()
 
@@ -22,13 +22,19 @@ class APKDownloader(private val url: String, private val outputFile: File) {
             override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
                 if (response.isSuccessful) {
                     response.body()?.let { body ->
+                        val contentLength = body.contentLength()
                         try {
                             val inputStream = body.byteStream()
                             val outputStream = FileOutputStream(outputFile)
                             val buffer = ByteArray(4096)
                             var bytesRead: Int
+                            var totalBytesRead: Long = 0
+
                             while (inputStream.read(buffer).also { bytesRead = it } != -1) {
                                 outputStream.write(buffer, 0, bytesRead)
+                                totalBytesRead += bytesRead
+                                val progress = (totalBytesRead * 100 / contentLength).toInt()
+                                onProgress(progress)
                             }
                             outputStream.flush()
                             outputStream.close()

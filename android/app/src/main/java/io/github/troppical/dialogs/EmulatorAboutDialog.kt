3d52c736d4508834d394c4590d7d4c505b359e2b
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-05-31 21:14:03 +0530

    implement Install button logic

--- android/app/src/main/java/io/github/troppical/dialogs/EmulatorAboutDialog.kt
+++ android/app/src/main/java/io/github/troppical/dialogs/EmulatorAboutDialog.kt
@@ -14,10 +14,14 @@ import com.bumptech.glide.Glide
 import kotlinx.coroutines.*
 import io.github.troppical.R
 import io.github.troppical.network.GitHubReleaseFetcher
+import io.github.troppical.network.APKDownloader
+import io.github.troppical.network.APKInstaller
+import java.io.File
 
 class EmulatorAboutDialog(context: Context, private val item: HashMap<String, Any>) : BaseSheetDialog(context) {
 
     private val fetcherScope = CoroutineScope(Dispatchers.Main + Job())
+    private val downloadUrl: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,7 @@ class EmulatorAboutDialog(context: Context, private val item: HashMap<String, An
         val emulatorDesc = findViewById<TextView>(R.id.emulator_desc)
         val emulatorLogo = findViewById<ImageView>(R.id.emulator_logo)
         val emulatorLatestVersion = findViewById<TextView>(R.id.emulator_latest_version)
+        val installButton = findViewById<MaterialButton>(R.id.install)
 
         emulatorName.text = item["emulator_name"].toString()
         emulatorDesc.text = item["emulator_desc"].toString()
@@ -40,8 +45,8 @@ class EmulatorAboutDialog(context: Context, private val item: HashMap<String, An
                 // Update the UI with the fetched tag name
                 emulatorLatestVersion.text = tagName
 
-                // Set the download link if the artifact is found
                 if (directLink != null) {
+                    downloadUrl = directLink 
                     Log.w("EmulatorAboutDialog", "Direct download link is $directLink")
                 } else {
                     Log.e("EmulatorAboutDialog", "Artifact not found")
@@ -54,6 +59,21 @@ class EmulatorAboutDialog(context: Context, private val item: HashMap<String, An
                 fetcher.close()
             }
         }
+        
+        installButton.setOnClickListener {
+          if (downloadUrl != null) {
+              val outputFile = File(context.filesDir, item["emulator_artifact_name"].toString())
+              val downloader = APKDownloader(downloadUrl, outputFile)
+              downloader.download { success ->
+                  if (success) {
+                      val installer = APKInstaller(context)
+                      installer.install(outputFile)
+                  } else {
+                      // TODO: Handle download failure
+                  }
+              } 
+           }     
+        }          
     }
 
     override fun onStop() {

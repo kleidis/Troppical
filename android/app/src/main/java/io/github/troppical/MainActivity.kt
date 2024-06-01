604179bb1103d2c0e66f8166f3fa7b2ba27c1a3d
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-06-01 19:22:21 +0530

    implement swipe refreshing bar

--- android/app/src/main/java/io/github/troppical/MainActivity.kt
+++ android/app/src/main/java/io/github/troppical/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.GridView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.google.android.material.color.MaterialColors
 import io.github.troppical.network.RequestNetwork
 import io.github.troppical.network.RequestNetworkController
 import io.github.troppical.adapters.EmulatorAdapter
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private var listmap: ArrayList<HashMap<String, Any>> = ArrayList()
     private lateinit var netRequestListener: RequestNetwork.RequestListener
+    private var isRefreshing: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
                 listmap = Gson().fromJson(_response, object : TypeToken<ArrayList<HashMap<String, Any>>>() {}.type)
                 val gridEmulators: GridView = findViewById(R.id.grid_emulators)
                 gridEmulators.adapter = EmulatorAdapter(this@MainActivity, this@MainActivity, listmap)
+                isRefreshing = false
             }
 
             override fun onErrorResponse(_param1: String, _param2: String) {
@@ -39,6 +42,34 @@ class MainActivity : AppCompatActivity() {
             "",
             netRequestListener
         )
+
+        binding.swipeRefresh.apply {
+            // Add swipe down to refresh gesture
+            setOnRefreshListener {
+                isRefreshing = true
+                net.startRequestNetwork(
+                    RequestNetworkController.GET,
+                    "https://script.googleusercontent.com/macros/echo?user_content_key=Hw-G9S_OHELhOUAsT-oQr8ux2HPMIpva3U1w0Su7P1ZYrr1ngXyqlN6LBhfev1taFoRtJ07w_KDhWVbMaBaeJ3c86H4e0k8Xm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnL69XsVZDOhipZMwrhs3JioNozSVnp4Chm6SveAF_nlUSMgTaOh-zk0bQ5F9LtyaiRZKic-heYuYVV866SySaVfv-0TkTPKcCtz9Jw9Md8uu&lib=MmjrdpKGbUxdyxLDAqWkoFhoZjK-0W8qS",
+                    "",
+                    netRequestListener
+                )
+            }
+
+            // Set theme color to the refresh animation's background
+            setProgressBackgroundColorSchemeColor(
+                MaterialColors.getColor(
+                    binding.swipeRefresh,
+                    com.google.android.material.R.attr.colorPrimary
+                )
+            )
+            setColorSchemeColors(
+                MaterialColors.getColor(
+                    binding.swipeRefresh,
+                    com.google.android.material.R.attr.colorOnPrimary
+                )
+            )
+            binding.swipeRefresh.isRefreshing = isRefreshing
+        }
 
         clearApkFiles(filesDir)
     }

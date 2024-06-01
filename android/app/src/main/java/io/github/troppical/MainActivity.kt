52a6211c85cd69c16c058ede4219bb54a467d3e9
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-06-01 22:20:47 +0530

    correctly handle swipe refresh bar

--- android/app/src/main/java/io/github/troppical/MainActivity.kt
+++ android/app/src/main/java/io/github/troppical/MainActivity.kt
@@ -16,11 +16,13 @@ class MainActivity : AppCompatActivity() {
 
     private var listmap: ArrayList<HashMap<String, Any>> = ArrayList()
     private lateinit var netRequestListener: RequestNetwork.RequestListener
-    private var isRefreshing: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val swipeRefresh = findViewById<SwipeRefreshLayout>(R.id.swipe_refresh)
+        swipeRefresh.isRefreshing = true
 
         netRequestListener = object : RequestNetwork.RequestListener {
             override fun onResponse(_param1: String, _param2: String, _param3: HashMap<String, Any>) {
@@ -28,11 +30,12 @@ class MainActivity : AppCompatActivity() {
                 listmap = Gson().fromJson(_response, object : TypeToken<ArrayList<HashMap<String, Any>>>() {}.type)
                 val gridEmulators: GridView = findViewById(R.id.grid_emulators)
                 gridEmulators.adapter = EmulatorAdapter(this@MainActivity, this@MainActivity, listmap)
-                isRefreshing = false
+                swipeRefresh.isRefreshing = false
             }
 
             override fun onErrorResponse(_param1: String, _param2: String) {
                 // TODO: Handle this situation
+                swipeRefresh.isRefreshing = true
             }
         }
 
@@ -44,12 +47,10 @@ class MainActivity : AppCompatActivity() {
             netRequestListener
         )
 
-        val swipeRefresh = findViewById<SwipeRefreshLayout>(R.id.swipe_refresh)
-
         swipeRefresh.apply {
             // Add swipe down to refresh gesture
             setOnRefreshListener {
-                isRefreshing = true
+                swipeRefresh.isRefreshing = true
                 net.startRequestNetwork(
                     RequestNetworkController.GET,
                     "https://script.googleusercontent.com/macros/echo?user_content_key=Hw-G9S_OHELhOUAsT-oQr8ux2HPMIpva3U1w0Su7P1ZYrr1ngXyqlN6LBhfev1taFoRtJ07w_KDhWVbMaBaeJ3c86H4e0k8Xm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnL69XsVZDOhipZMwrhs3JioNozSVnp4Chm6SveAF_nlUSMgTaOh-zk0bQ5F9LtyaiRZKic-heYuYVV866SySaVfv-0TkTPKcCtz9Jw9Md8uu&lib=MmjrdpKGbUxdyxLDAqWkoFhoZjK-0W8qS",
@@ -71,7 +72,6 @@ class MainActivity : AppCompatActivity() {
                     com.google.android.material.R.attr.colorOnPrimary
                 )
             )
-            swipeRefresh.isRefreshing = isRefreshing
         }
 
         clearApkFiles(filesDir)

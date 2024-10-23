b6905a3c0880e17bc4fa5c5ac5c2affd05c5cc60
Kleidis <167202775+kleidis@users.noreply.github.com>
2024-10-23 14:43:15 +0200

    ui: Improve the `qt_button_click` and `qt_index_switcher` functions

--- ui/ui_main.py
+++ ui/ui_main.py
@@ -10,6 +10,8 @@ from init_instances import inst
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.shared_thread = None  # Thrad used for secondary function pirposes
+
         self.setWindowTitle(f'Troppical - {"version"}')  # Window name with version
         self.setCentralWidget(QWidget(self))  # Set a central widget
         self.layout = QStackedLayout(self.centralWidget())  # Set the layout on the central widget
@@ -19,36 +21,36 @@ class MainWindow(QMainWindow):
         #   icon_path = os.path.join(sys._MEIPASS, 'icon.ico')
         #   self.setWindowIcon(QIcon(icon_path))
         self.load_stylesheet()
-        # Initialize the first page
-        self.layout.addWidget(inst.wel.welcomePage)
-
-        # Connect buttons after initialization
+        self.widget_2_layout()
         self.connect_buttons()
-
-        self.shared_thread = None  # Initialize shared_thread to None
 
     def connect_buttons(self):
         # Connect buttons to the qt_button_click method
         inst.wel.manageButton.clicked.connect(self.qt_button_click)
+        inst.sel.nextButton.clicked.connect(self.qt_button_click)
         inst.act.installButton.clicked.connect(self.qt_button_click)
         inst.act.updateButton.clicked.connect(self.qt_button_click)
         inst.act.uninstallButton.clicked.connect(self.qt_button_click)
         inst.install.install_emu_button.clicked.connect(self.qt_button_click)
         inst.act.backButton.clicked.connect(self.qt_button_click)
+        inst.finish.finishButton.clicked.connect(self.qt_button_click)
+        inst.finish.installAnotherButton.clicked.connect(self.qt_button_click)
 
     def widget_2_layout(self):
         # Add actual pages instead of placeholders
-        self.layout.addWidget(inst.sel.emulatorSelectPage)
         self.layout.addWidget(inst.wel.welcomePage)
+        self.layout.addWidget(inst.sel.emulatorSelectPage)
+        self.layout.addWidget(inst.act.actPage)
         self.layout.addWidget(inst.install.installPage)
         self.layout.addWidget(inst.bar.progressBarPage)
         self.layout.addWidget(inst.finish.finishPage)
 
-    def initialize_page(self, index):
+    def qt_index_switcher(self, index):
         # Mapping of index to page attributes and instances
         page_map = {
+            0: ('welcome_page', inst.wel, 'welcomePage'),
             1: ('selection_page', inst.sel, 'emulatorSelectPage'),
-            2: ('welcome_page', inst.wel, 'welcomePage'),
+            2: ('act_page', inst.act, 'actPage'),
             3: ('install_page', inst.install, 'installPage'),
             4: ('progress_bar_page', inst.bar, 'progressBarPage'),
             5: ('finish_page', inst.finish, 'finishPage')
@@ -61,44 +63,62 @@ class MainWindow(QMainWindow):
             widget = getattr(instance, widget_name)
             self.layout.replaceWidget(self.layout.widget(index), widget)
             self.layout.setCurrentIndex(index)
-            print(f"Page {index} initialized: {attr_name}")
 
     def load_stylesheet(self):
         self.setStyleSheet(Style.dark_stylesheet)
 
     def qt_button_click(self):
-        self.widget_2_layout()
         button = self.sender()
 
         # Define a mapping of buttons to their actions
         button_actions = {
-            inst.wel.manageButton: 1,  # Assuming index 1 is the manage page
-            inst.act.installButton: 2,
+            inst.wel.manageButton: self.handle_manage,
+            inst.sel.nextButton: self.handle_select,
+            inst.act.installButton: self.handle_install,
             inst.act.updateButton: inst.main.emulator_updates,
             inst.act.uninstallButton: self.handle_uninstall,
-            inst.install.install_emu_button: 3,
-            inst.act.backButton: self.handle_back
+            inst.install.install_emu_button: self.handle_install_emu,
+            inst.finish.finishButton: self.handle_finish,
+            inst.finish.installAnotherButton: self.handle_finish,
+            inst.act.backButton: self.handle_back,
         }
 
         # Execute the corresponding action if the button is in the mapping
         action = button_actions.get(button)
         if isinstance(action, int):
-            self.initialize_page(action)
+            self.qt_index_switcher(action)
         elif callable(action):
             action()
 
     def handle_manage(self):
-        self.layout.setCurrentIndex(1)  # Assuming index 1 is the desired page
+        if not hasattr(self, 'emulator_database_initialized') or not self.emulator_database_initialized:
+            self.initialize_emulator_database()
+            self.emulator_database_initialized = True
+        inst.ui.qt_index_switcher(1)
+    def handle_select(self):
+        inst.main.set_emulator()
+        inst.ui.qt_index_switcher(2)
     def handle_install(self):
         inst.main.install_mode = "Install"
-        self.layout.setCurrentIndex(2)
+        inst.ui.qt_index_switcher(3)
         inst.main.Add_releases_to_combobox()
     def handle_uninstall(self):
         inst.main.install_mode = "Uninstall"  # Unused for now
         inst.main.uninstall()
     def handle_install_emu(self):
-        self.layout.setCurrentIndex(3)
+        inst.ui.qt_index_switcher(4)
         inst.main.Prepare_Download()
+    def handle_finish(self):
+        button = self.sender()
+        if button == inst.finish.installAnotherButton:
+            self.layout.setCurrentIndex(0)
+            inst.bar.downloadProgressBar.setValue(0)
+            inst.bar.extractionProgressBar.setValue(0)
+            setattr(inst.main, 'install_mode', None)
+            setattr(inst.main, 'selection', None)
+        elif button == inst.finish.finishButton:
+            self.close()
+
     def handle_back(self):
         current_index = self.layout.currentIndex()
         if current_index > 0:
@@ -150,3 +170,4 @@ class Worker(QObject):
         if self.task is not None:
             result = self.task(*self.args, **self.kwargs)
             self.finished.emit(result)
+

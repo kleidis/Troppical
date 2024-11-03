4b27b68429303546baa848975f9aeafc6ed68e4c
Kleidis <167202775+kleidis@users.noreply.github.com>
2024-11-03 22:28:42 +0100

    ui: Add mica backdrop support for Windows 11

--- stylesheet.py
+++ stylesheet.py
@@ -3,7 +3,7 @@
 class Style:
     dark_stylesheet = """
         QWidget {
-            background-color: rgb(32, 32, 32);
+            background-color: transparent;
             color: rgb(255, 255, 255);
             font-size: 17px;
             font-family: "Segoe UI Variable Small", serif;
@@ -11,7 +11,7 @@ class Style:
         }
         /*PUSHBUTTON*/
         QPushButton {
-            background-color: #323234;
+            background-color: rgba(50, 50, 52, 0.7);
             border: 1px solid rgb(68, 68, 68);
             border-radius: 7px;
             min-height: 38px;
@@ -19,19 +19,19 @@ class Style:
         }
 
         QPushButton:hover {
-            background-color: rgb(100, 100, 100);
+            background-color: rgba(100, 100, 100, 0.7);
             border: 1px solid rgba(255, 255, 255, 10);
         }
 
         QPushButton::pressed {
-            background-color: rgba(255, 255, 255, 7);
+            background-color: rgba(255, 255, 255, 0.07);
             border: 1px solid rgba(255, 255, 255, 13);
             color: rgba(255, 255, 255, 200);
         }
 
         QPushButton::disabled {
             color: rgb(150, 150, 150);
-            background-color: rgba(255, 255, 255, 13);
+            background-color: rgba(255, 255, 255, 0.13);
         }
         QLabel, QCheckBox {
             color: #ffffff;
@@ -41,12 +41,13 @@ class Style:
             border-radius: 5px;
             border: 0px solid rgba(255, 255, 255, 13);
             margin-top: 36px;
+            background-color: transparent;
         }
 
         QGroupBox::title {
             subcontrol-origin: margin;
             subcontrol-position: top left;
-            background-color: rgba(255, 255, 255, 16);
+            background-color: rgba(255, 255, 255, 0.16);
             padding: 7px 15px;
             margin-left: 5px;
             border-top-left-radius: 5px;
@@ -58,7 +59,7 @@ class Style:
         }
         /*COMBOBOX*/
         QComboBox {
-            background-color: rgb(66, 66, 66);
+            background-color: rgba(66, 66, 66, 0.7);
             border: 1px solid rgb(68, 68, 68);
             border-radius: 5px;
             padding-left: 10px;
@@ -67,12 +68,12 @@ class Style:
         }
 
         QComboBox:hover {
-            background-color: rgb(120, 120, 120);
+            background-color: rgba(120, 120, 120, 0.7);
             border: 1px solid rgba(255, 255, 255, 10);
         }
 
         QComboBox::pressed {
-            background-color: rgb(66, 66, 66);
+            background-color: rgba(66, 66, 66, 0.7);
             border: 1px solid #0078D4;
             color: rgba(255, 255, 255, 200);
         }
@@ -88,7 +89,7 @@ class Style:
 
         QComboBox:disabled {
             color: rgb(150, 150, 150);
-            background-color: rgba(255, 255, 255, 13);
+            background-color: rgba(255, 255, 255, 0.13);
             border: 1px solid rgba(255, 255, 255, 5);
         }
 
@@ -106,16 +107,16 @@ class Style:
             height: 22px;
             border-radius: 5px;
             border: 2px solid #848484;
-            background-color: rgba(255, 255, 255, 0);
+            background-color: transparent;
             margin-right: 5px;
         }
 
         QCheckBox::indicator:hover {
-            background-color: rgba(255, 255, 255, 16);
+            background-color: rgba(255, 255, 255, 0.16);
         }
 
         QCheckBox::indicator:pressed {
-            background-color: rgba(255, 255, 255, 20);
+            background-color: rgba(255, 255, 255, 0.20);
             border: 2px solid #434343;
         }
 
@@ -135,18 +136,18 @@ class Style:
 
         QCheckBox::indicator:disabled {
             border: 2px solid #646464;
-            background-color: rgba(255, 255, 255, 0);
+            background-color: transparent;
         }
         /*PROGRESSBAR*/
         QProgressBar {
-            background-color: qlineargradient(spread:reflect, x1:0.5, y1:0.5, x2:0.5, y2:1, stop:0.119403 rgba(255, 255, 255, 250), stop:0.273632 rgba(0, 0, 0, 0));
+            background-color: qlineargradient(spread:reflect, x1:0.5, y1:0.5, x2:0.5, y2:1, stop:0.119403 rgba(255, 255, 255, 0.25), stop:0.273632 rgba(0, 0, 0, 0));
             border-radius: 2px;
             min-height: 4px;
             max-height: 4px;
         }
 
         QProgressBar::chunk {
-            background-color: #0078D4;;
+            background-color: #0078D4;
             border-radius: 2px;
         }
         /*MENU*/
@@ -167,11 +168,11 @@ class Style:
         }
 
         QMenu::item:selected {
-            background-color: rgba(255, 255, 255, 16);
+            background-color: rgba(255, 255, 255, 0.16);
         }
 
         QMenu::item:pressed {
-            background-color: rgba(255, 255, 255, 10);
+            background-color: rgba(255, 255, 255, 0.10);
         }
 
         QMenu::right-arrow {
@@ -190,7 +191,7 @@ class Style:
         }
         /*LINEEDIT*/
         QLineEdit {
-            background-color: #323234;
+            background-color: rgba(50, 50, 52, 0.7);
             border: 1px solid rgb(68, 68, 68);
             font-size: 16px;
             font-family: "Segoe UI", serif;
@@ -202,14 +203,14 @@ class Style:
         }
 
         QLineEdit:hover {
-            background-color: rgb(120, 120, 120);
+            background-color: rgba(120, 120, 120, 0.7);
             border: 1px solid rgba(255, 255, 255, 10);
             border-bottom: 1px solid rgba(255, 255, 255, 150);
         }
 
         QLineEdit:focus {
             border-bottom: 2px solid #0078D4;
-            background-color: rgb(100, 100, 100);
+            background-color: rgba(100, 100, 100, 0.7);
             border-top: 1px solid rgba(255, 255, 255, 13);
             border-left: 1px solid rgba(255, 255, 255, 13);
             border-right: 1px solid rgba(255, 255, 255, 13);
@@ -217,12 +218,12 @@ class Style:
 
         QLineEdit:disabled {
             color: rgb(150, 150, 150);
-            background-color: rgba(255, 255, 255, 13);
+            background-color: rgba(255, 255, 255, 0.13);
             border: 1px solid rgba(255, 255, 255, 5);
         }
         /*TREEWIDGET*/
         QTreeWidget {
-            background-color: #2D2D2D; 
+            background-color: transparent;
             color: white;
         }
         QTreeView {
@@ -239,7 +240,7 @@ class Style:
         }
 
         QTreeView::item:selected {
-            background-color: #2D2D2D;
+            background-color: rgba(45, 45, 45, 0.7);
             border-radius: 0px;
             margin-bottom: 3px;
             padding-left: 0px;
@@ -267,13 +268,13 @@ class Style:
         }
         /*SCROLLVERTICAL*/
         QScrollBar:vertical {
-            border: 6px solid rgba(255, 255, 255, 0);
+            border: 6px solid transparent;
             margin: 14px 0px 14px 0px;
             width: 16px;
         }
 
         QScrollBar:vertical:hover {
-            border: 5px solid rgba(255, 255, 255, 0);
+            border: 5px solid transparent;
         }
 
         QScrollBar::handle:vertical {
@@ -316,13 +317,13 @@ class Style:
 
         /*SCROLLHORIZONTAL*/
         QScrollBar:horizontal {
-            border: 6px solid rgba(255, 255, 255, 0);
+            border: 6px solid transparent;
             margin: 0px 14px 0px 14px;
             height: 16px;
         }
 
         QScrollBar:horizontal:hover {
-            border: 5px solid rgba(255, 255, 255, 0);
+            border: 5px solid transparent;
         }
 
         QScrollBar::handle:horizontal {
